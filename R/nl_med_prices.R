181a0287c620a9fb83156685026ec9e4d4248ba7
stijnbpeeters <s.b.peeters@eshpm.eur.nl>
2024-12-05 17:31:54 +0100

    Addition of nl_pat_fam_prices function

--- R/nl_med_prices.R
+++ R/nl_med_prices.R
@@ -6,7 +6,7 @@
 #' @param category The category of prices that should be included (one or more categories), default is including all categories
 #' @param unit The reference price that should be included (one or multiple reference prices),  default is including the whole dataframe
 #' @param currency The currency of the output of the prices. A decision can be made between EUR and INT$, the default is EUR.
-#' @return A dataframe with the Medical Reference prices of the Dutch Costing Manual for the specified years.
+#' @return A dataframe or value with the Medical Reference price(s) of the Dutch Costing Manual for the specified years
 #' @examples
 #' Example usage of the nl_med_prices function
 #' nl_med_prices(year = 2019, category = "Nursing", unit = "Nursing day excluding personnel costs, hospital")
@@ -20,12 +20,12 @@ nl_med_prices <- function(
     unit = "all"){
 
   # read in the dataset
-  df <- df_rp_medical
+  df <- tatooheene::df_rp_medical
 
   # If specified, select the specified years, or all years if not specified
   if(year != "all"){
     df <-  df |>
-      dplyr::select("Category", "Unit", all_of(c(as.character(year))))
+      dplyr::select("Category", "Unit", dplyr::all_of(c(as.character(year))))
   }
 
   #If currency is INT$, change output
--- R/nl_pat_fam_prices.R
+++ R/nl_pat_fam_prices.R
@@ -1,7 +1,15 @@
 #' A function to download the Patient & Family Reference prices of the Dutch Costing Manual for one or multiple years
+#'
+#' This function downloads the Patient & Family Reference prices of the Dutch Costing Manual for one or multiple years. The prices are available in Euro (EUR) or International Dollar (INT$).
+#'
 #' @param year The year of which the reference price should be downloaded, multiple years are possible, default is the whole dataset (year = "all")
 #' @param category The category of prices that should be included (one or more categories), default is including all categories
+#' @param currency The currency of the output of the prices. A decision can be made between EUR and INT$, the default is EUR.
 #' @param unit The reference price that should be included (one or multiple reference prices),  default is including the whole data frame
+#' @return A dataframe or value with the Patient & Family Reference price(s) of the Dutch Costing Manual for the specified years
+#' @examples
+#' Example usage of the nl_pat_fam_prices function
+#' nl_pat_fam_prices(year = 2019, category = "Transportation", unit = "Car, cost per kilometer")
 #' @keywords Generic, Costing Manual, Dutch Reference Prices, Patient & Family Prices
 #' @export nl_pat_fam_prices
 
@@ -11,44 +19,39 @@ nl_pat_fam_prices <- function(
     currency = "EUR",
     unit = "all"){
 
-  # Filepath
-  ##  Get system year - 1 year for update
-  update_year <- as.numeric(format(Sys.Date(), "%Y")) - 1
-  new_filepath_patient <- paste0("data/RP_patient_", update_year, ".rds")
-
   # Read dataframe
-  df_patient <- readRDS(new_filepath_patient)
+  df <- tatooheene::df_rp_patient
 
   if(year != "all"){
-    df_patient <-  df_patient |>
-      dplyr::select("Category", "Unit", all_of(c(as.character(year))))
+    df <-  df |>
+      dplyr::select("Category", "Unit", dplyr::all_of(c(as.character(year))))
   }
 
   #If currency is INT$, change output
   if(currency == "INT$"){
-    df_ppp <- f_factor_EurDutch_to_IntDollar()
+    df_ppp <- tatooheene::nl_ppp()
     df_ppp <- df_ppp |>
       dplyr::filter(as.numeric(Year) >= 2022) |>
       tidyr::pivot_wider(names_from = "Year", values_from = "PPP")
 
     df_ppp <- df_ppp[, rev(seq_len(ncol(df_ppp)))]
 
-    common_years <- intersect(colnames(df_patient), colnames(df_ppp))
+    common_years <- intersect(colnames(df), colnames(df_ppp))
 
-    df_patient[, common_years] <- sweep(df_patient[, common_years], 2, as.numeric(df_ppp[, common_years]), `*`)
+    df[, common_years] <- sweep(df[, common_years], 2, as.numeric(df_ppp[, common_years]), `*`)
   }
 
 
   # If specified, filter based on category
   if(category != "all"){
-    df_patient <- df_patient |>
+    df <- df |>
       dplyr::filter(Category %in% category)
   }
 
   if(unit != "all"){
-    df_patient <- df_patient |>
+    df <- df |>
       dplyr::filter(Unit %in% unit)
   }
 
-  return(df_patient)
+  return(df)
 }
