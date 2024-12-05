6664c6b5698d872daa2d64a7f53b1e20eedf870a
stijnbpeeters <s.b.peeters@eshpm.eur.nl>
2024-12-05 17:56:15 +0100

    Addition of Frederick's pretty price function

---
+++ R/pretty_price.R
@@ -0,0 +1,21 @@
+#' A function to write pretty prices in `bookdown` reports
+#'
+#' This function writes pretty prices in `bookdown` reports. The function uses the `formatC()` function to format the number and adds the currency to the end of the number.
+#' @param x A number to be printed
+#' @param currency The name of the currency
+#' @param digi Number of digits
+#' @param ... Extra arguments for `formatC()`
+#' @return A pretty price with the currency
+#' @examples
+#' p_price(1000, currency = "EUR")
+#' @export p_price
+#' @importFrom base formatC
+
+pretty_price <- function(x, digi = 2, currency = "EUR", ...){
+
+  paste(formatC(round(x, digits = digi),
+                big.mark = ",",
+                format = "f",
+                digits = digi, ...), currency)
+
+}
