a98f6fa76aedb528c718a394a05196c2940ff623
Eline Krijkamp <e.krijkamp@erasmusmc.nl>
2025-12-04 10:20:14 +0100

    Simplify discount_rate parameter documentation
    Updated the documentation for the discount_rate parameter in apply_discounting.R to provide a clearer and more concise description of acceptable values, removing redundant details and improving readability.

--- R/apply_discounting.R
+++ R/apply_discounting.R
@@ -4,13 +4,8 @@
 #' `r lifecycle::badge("experimental")`
 #' The apply_discounting function is designed to calculate the net present value of future costs or effects using a constant  discount rate, following the Dutch guidelines for economic evaluations in health care.  (section 2.6.1.2 version 2024). Here's a breakdown of how the function works:
 #'@param values A numeric (vector of) costs or effects over time (one value per period).
-#'@param discount_rate Specifies the discount rate to be used. It can be "costs", "effects" or a custom numeric value
-#' #  Acceptable values are:
-#' \itemize{
-#'     \item \code{"costs"} — applies a 3\% (0.03) annual discount rate
-#'     \item \code{"effects"} — applies a 1.5\% (0.015) annual discount rate
-#'     \item A numeric value (e.g., \code{0.04}) — applies a custom annual discount rate
-#'   }
+#' @param discount_rate Specifies the discount rate to be used. Acceptable values are:
+#'   "costs" (0.03), "effects" (0.015), or a custom numeric value such as 0.04.
 #'@param times A numeric (vector of) time points indicating the time used for the discounting. The length must match the length of the values vector. Since the default discounting is annual, the time points should be in years. The length of this vector should be the same as the length of the `values` vector. When the first year is not discounted, the time points should start at 0 (e.g., c(0, 1, 2) for three years with NO discounting in the first year). When the first year is discounted, the time points should start at 1 (e.g., c(1, 2, 3) for three years WITH discounting in the first year). In case costs or effects are accrued in time steps other they annual, the time points should be adjusted accordingly, see more details in the vignettes of this package about discounting.
 #'@param aggregate A logical: indicating whether to sum the discounted values. Default is FALSE.
 #'@param digits A numeric value to indicate the number of digits to round the value. Default is 3 digits
