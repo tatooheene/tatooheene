0bec99315508e052a134dfc1ac2436483509a017
Eline Krijkamp <e.krijkamp@erasmusmc.nl>
2025-12-05 13:09:15 +0100

    Rename and update Sick-Sicker model dataset documentation

--- R/data_model_sick_sicker.R
+++ R/data_model_sick_sicker.R
@@ -1,4 +1,4 @@
-#' Example dataset: # data_model_output_sick_sicker
+#' Example dataset: data_model_output_sick_sicker
 #'
 #' @description
 #' A dataset containing model output objects from the **Sick-Sicker** example model developed by the **DARTH (Decision Analysis in R for Technologies in Health)** workgroup. This dataset includes the results from both annual and monthly Markov model runs, specifically the Markov trace files and related output objects produced during simulation.
@@ -22,7 +22,7 @@
 #' This dataset represents a subset of model outputs intended for demonstrating the discounting in this package.
 #' The full model is described in the DARTH “Sick-Sicker” example.
 #'
-#'#' Source code can be found here: https://github.com/DARTH-git/cohort-modeling-tutorial-intro
+#' Source code can be found here: https://github.com/DARTH-git/cohort-modeling-tutorial-intro
 #' For the full reference read: Alarid-Escudero F, Krijkamp EM, Enns EA, Yang A, Hunink MGM, Pechlivanoglou P, Jalal H. An Introductory Tutorial on Cohort State-Transition Models in R Using a Cost-Effectiveness Analysis Example. Medical Decision Making, 2023;43(1):3-20. https://doi.org/10.1177/0272989X221103163
 #'
 #'
@@ -30,8 +30,10 @@
 #'
 #' @examples
 #' # Load the dataset
-#' # data("data_model_output_sick_sicker")
+#' data("data_model_output_sick_sicker.rda")
 #'
+#'# Load list in global environment
+#' list2env(data_model_output_sick_sicker, envir = .GlobalEnv)
 #' # Explore the available objects
 #' ls(pattern = "l_|v_")
 #'
@@ -45,28 +47,25 @@
 #' dim(l_m_M_annual[[1]])
 #' dim(l_m_M_monthly[[1]])
 #'
-#' # Example: compute mean annual utility
-#' mean(unlist(l_u_annual))
-#'
 #' # Apply the half cycle correction
 #' ## Loop through each strategy and calculate total utilities and costs ----
 #'
-
+#' v_tot_qaly <- v_tot_cost <- c()
 #'for (i in 1:length(v_names_str)) {
-#'  v_u_str <- l_u[[i]]   # select the vector of state utilities for the i-th strategy
-#'  v_c_str <- l_c[[i]]   # select the vector of state costs for the i-th strategy
+#'  v_u_str <- l_u_monthly[[i]]   # select the vector of state utilities for the i-th strategy
+#'  v_c_str <- l_c_monthly[[i]]   # select the vector of state costs for the i-th strategy
 #'
 #'   ###* Expected QALYs and costs per cycle
 #'   ##* Vector of QALYs and Costs
 #'   #* Apply state rewards
-#'   v_qaly_str <- l_m_M[[i]] %*% v_u_str # sum the utilities of all states for each cycle
-#'   v_cost_str <- l_m_M[[i]] %*% v_c_str # sum the costs of all states for each cycle
+#'   v_qaly_str <- l_m_M_monthly[[i]] %*% v_u_str # sum the utilities of all states for each cycle
+#'   v_cost_str <- l_m_M_monthly[[i]] %*% v_c_str # sum the costs of all states for each cycle
 #'
 #'   ###*  Total expected QALYs and costs per strategy and apply half-cycle correction (if applicable)
 #'   #* QALYs
-#'   v_tot_qaly[i] <- t(v_qaly_str) %*% v_wcc
+#'   v_tot_qaly[i] <- t(v_qaly_str) %*% v_wcc_monthly
 #'   #* Costs
-#'   v_tot_cost[i] <- t(v_cost_str) %*% v_wcc
+#'   v_tot_cost[i] <- t(v_cost_str) %*% v_wcc_monthly
 #' }
 
 #'
